99a4d06cd055f85442bce5923ee23f2d6f4d03b6
Nurassyl Absamat <absamatnur@gmail.com>
2020-08-08 18:16:36 +0600

    Admin middlware for channel

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class ChannelController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('admin');
+    }
     /**
      * Display a listing of the resource.
      *
